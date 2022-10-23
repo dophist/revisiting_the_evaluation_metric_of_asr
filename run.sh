cddde4f70ef0f7ba768b3bc9214ba253d9e8ed7d
dophist <jerry.jiayu.du@gmail.com>
2022-10-23 20:31:28 +0800

    add CER

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-./error_rate  -m TER  --ref data/ref.txt  --hyp data/hyp.txt  DETAILS_TER.txt | tee RESULTS_TER.txt
+#./error_rate  -t char  -m TER  --ref data/ref.txt  --hyp data/hyp.txt  DETAILS_CER.txt | tee RESULTS_CER.txt
+./error_rate  -m TER  --ref data/ref.txt  --hyp data/hyp.txt  DETAILS_WER.txt | tee RESULTS_WER.txt
 ./error_rate  -m NID  --ref data/ref.txt  --hyp data/hyp.txt  DETAILS_NID.txt | tee RESULTS_NID.txt
-
