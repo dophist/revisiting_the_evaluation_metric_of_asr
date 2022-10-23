5ee9c9f08cbb2a2f50cb977df9fa2420fc8af61e
dophist <jerry.jiayu.du@gmail.com>
2022-10-23 19:37:56 +0800

    add --metric (-m) option

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-./error_rate -x data/ref.txt -y data/hyp.txt DETAILS.txt
+./error_rate  -m TER  --ref data/ref.txt  --hyp data/hyp.txt  DETAILS_TER.txt | tee RESULTS_TER.txt
+./error_rate  -m NID  --ref data/ref.txt  --hyp data/hyp.txt  DETAILS_NID.txt | tee RESULTS_NID.txt
+
