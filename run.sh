064203c2e600927291b102227afd5ce3a307c836
dophist <jerry.jiayu.du@gmail.com>
2022-10-25 19:04:50 +0800

    use nargs mode for args.metrics option

--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-#./error_rate  -t char  -m TER  --ref data/ref.txt  --hyp data/hyp.txt  DETAILS_CER.txt | tee RESULTS_CER.txt
-./error_rate  -m TER  --ref data/ref.txt  --hyp data/hyp.txt  DETAILS_WER.txt | tee RESULTS_WER.txt
-./error_rate  -m NID  --ref data/ref.txt  --hyp data/hyp.txt  DETAILS_NID.txt | tee RESULTS_NID.txt
+./error_rate  -m TER NID  --ref data/ref.txt  --hyp data/hyp.txt  DETAILS.txt | tee RESULTS.txt
