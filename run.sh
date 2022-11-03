6cb71aa75232b47d48f8ce8cab9b6baa05536458
dophist <jerry.jiayu.du@gmail.com>
2022-11-03 20:52:09 +0800

    add lm downloading

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
+
+if [[ ! -f lm/ngram/2gram.trie ]]; then
+	echo "Downloading language model for ASR evaluation ..."
+	wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/2gram.trie  -O lm/ngram/2gram.trie
+	echo "Language model downloaded."
+fi
+
 ./error_rate  -m TER NID  --lm lm/ngram/2gram.trie  --ref data/ref.txt  --hyp data/hyp.txt  DETAILS.txt | tee RESULTS.txt
