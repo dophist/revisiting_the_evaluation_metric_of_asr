854c35f53175d49a40c57df2e01787133ff51796
dophist <jerry.jiayu.du@gmail.com>
2022-11-03 19:58:01 +0800

    add comments for KenLM installation

--- train_lm.sh
+++ train_lm.sh
@@ -1,3 +1,5 @@
+# To install KenLM training tool, follow https://github.com/kpu/kenlm
+
 text=lm/text.txt
 arpa=lm/2gram.arpa
 trie=lm/2gram.trie
@@ -5,6 +7,5 @@ trie=lm/2gram.trie
 # Train ARPA
 cat $text | lmplz -o 2 > $arpa
 
-# Convert ARPA to foward-8bit backoff-8bit quantized trie
+# Convert ARPA to binary trie, with 8bit-foward 8bit-backoff quantization
 build_binary -q 8 -b 8 trie $arpa $trie
-
