4e213a0d6fc5915b595f0e0cded4f7a74a553107
dophist <jerry.jiayu.du@gmail.com>
2022-11-03 19:59:35 +0800

    add initial TN for wikitext-103, pretty rough

---
+++ lm/tn.py
@@ -0,0 +1,25 @@
+import string
+
+puncts_to_remove = string.punctuation.replace("'", '')
+puncts_trans = str.maketrans(puncts_to_remove, ' ' * len(puncts_to_remove), '')
+
+with open('text.txt.pre_tn', 'r') as fi, open('text.txt', 'w+') as fo:
+    for line in fi:
+        line = line.strip()
+        if not line:
+            continue
+
+        if line.startswith('='): # titles '= Valkyria Chronicles III =' '= = Gameplay = ='
+            continue
+
+        sentences = line.split(' . ')
+        for s in sentences:
+            s = s.translate(puncts_trans)
+            s = s.replace(" 's", "'s")
+            s = ' '.join(s.split())
+            s = s.upper()
+            s = s.strip()
+            if s:
+                fo.write(s + '\n')
+
+
