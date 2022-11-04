f171f3488c7da44e0418e7d34aa6d64e352b28ef
dophist <jerry.ejwt@qq.com>
2022-11-05 01:14:55 +0800

    rm download.sh

--- lm/download_text.sh
+++
@@ -1,2 +0,0 @@
-ossutil64 cp oss://swaphub/wikitext-103/wiki.train.tokens wiki.train.tokens
-ossutil64 cp oss://swaphub/wikitext-103-raw/wiki.train.raw wiki.train.raw
--- lm/run.sh
+++ lm/run.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-sh download_text.sh
+# prepare text
+ossutil64 cp  oss://swaphub/asr_metric_assets/wikitext-103/wiki.train.tokens   wiki.train.tokens
+ossutil64 cp  oss://swaphub/asr_metric_assets/wikitext-103-raw/wiki.train.raw  wiki.train.raw
+ln -s wiki.train.raw text.txt
 
+# textnorm
 tn.py
 
+# train tokenizer and tokenize text
 python ops/tokenizer_train.py -c tokenizer.yaml -i text.txt.tn -o tokenizer
 python ops/tokenizer.py -x encode -m tokenizer.model -i text.txt.tn -o text.txt.tn.tokenize
 
+# train subwords ngram model
 sh train_ngram.sh
 
