4d6b6c7bdb508acedaa5bf0511ce4c749f33d2f9
dophist <jerry.ejwt@qq.com>
2022-11-05 01:22:34 +0800

    clean run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
 if [[ ! -f lm/4gram.trie ]]; then
-	echo "Downloading language model for ASR evaluation ..."
+    echo "Downloading language model for ASR evaluation ..."
     #wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/2gram_word.trie -O lm/2gram_word.trie
-	wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/4gram.trie -O lm/4gram.trie
-	#wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/6gram.trie -O lm/6gram.trie
-	echo "Language model downloaded."
+    wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/4gram.trie -O lm/4gram.trie
+    #wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/6gram.trie -O lm/6gram.trie
+    echo "Language model downloaded."
 fi
 
 
-./error_rate  -m TER  --ref ref.txt  --hyp hyp.txt  DETAILS.txt | tee RESULTS.txt
-./error_rate  -m TER NID  --lm lm/4gram.trie  -t lm/tokenizer.model  --ref ref.txt  --hyp hyp.txt  DETAILS.txt | tee RESULTS.txt
+# word-token TER & mTER
+./error_rate  -m TER  --ref ref.txt  --hyp hyp.txt  DETAILS0.txt | tee RESULTS0.txt
+
+# subword-token NIER
+./error_rate  -m TER NID  -t lm/tokenizer.model  --lm lm/4gram.trie  --ref ref.txt  --hyp hyp.txt  DETAILS1.txt | tee RESULTS1.txt
 
 ## run examples in paper
 #./error_rate  -m TER  --ref data/paper/ref.txt  --hyp data/paper/hyp.txt  tmp.txt
+
