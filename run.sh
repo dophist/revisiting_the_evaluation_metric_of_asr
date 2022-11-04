d42d0760338fb89837a86275874301287e2d0a58
dophist <jerry.ejwt@qq.com>
2022-11-05 01:08:05 +0800

    misc

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-if [[ ! -f lm/ngram/ngram.trie ]]; then
+if [[ ! -f lm/4gram.trie ]]; then
 	echo "Downloading language model for ASR evaluation ..."
+    #wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/2gram_word.trie -O lm/2gram_word.trie
 	wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/4gram.trie -O lm/4gram.trie
+	#wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/6gram.trie -O lm/6gram.trie
 	echo "Language model downloaded."
 fi
 
+
+./error_rate  -m TER  --ref ref.txt  --hyp hyp.txt  DETAILS.txt | tee RESULTS.txt
 ./error_rate  -m TER NID  --lm lm/4gram.trie  -t lm/tokenizer.model  --ref ref.txt  --hyp hyp.txt  DETAILS.txt | tee RESULTS.txt
 
 ## run examples in paper
