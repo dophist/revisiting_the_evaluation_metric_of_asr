797446fb80bb9576f421a21f94920a785e227536
dophist <jerry.jiayu.du@gmail.com>
2022-11-05 15:31:34 +0800

    refine run.sh

--- run.sh
+++ run.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
 
-##echo "Downloading language model for ASR evaluation ..."
-#wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/ngram_word/2gram.trie        -O lm/ngram_word/2gram.trie
-#wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/ngram_subword256/4gram.trie  -O lm/ngram_subword256/4gram.trie
-#wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/ngram_subword256/6gram.trie  -O lm/ngram_subword256/6gram.trie
-#wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/ngram_subword1024/4gram.trie -O lm/ngram_subword1024/4gram.trie
-#wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/ngram_subword1024/6gram.trie -O lm/ngram_subword1024/6gram.trie
-##echo "Language model downloaded."
+tokenizer=lm/tokenizer256/tokenizer.model
+lm=lm/ngram_subword256/6gram.trie
+
+if [[ ! -f $lm ]]; then
+    #echo "Downloading language model for ASR evaluation ..."
+    mkdir -p lm/ngram_word
+    wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/ngram_word/2gram.trie        -O lm/ngram_word/2gram.trie
+
+    mkdir -p lm/ngram_subword
+    wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/ngram_subword256/4gram.trie  -O lm/ngram_subword256/4gram.trie
+    wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/ngram_subword256/6gram.trie  -O lm/ngram_subword256/6gram.trie
+    wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/ngram_subword1024/4gram.trie -O lm/ngram_subword1024/4gram.trie
+    wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/ngram_subword1024/6gram.trie -O lm/ngram_subword1024/6gram.trie
+    #echo "Language model downloaded."
+fi
+
 
 ref=ref.txt
 hyp=hyp.txt
 
-lm=lm/ngram_subword256/6gram.trie
-tokenizer=lm/tokenizer256/tokenizer.model
-
-# word-token TER & mTER
+# word-level TER & mTER
 ./error_rate  -m TER  -t word  --ref $ref  --hyp $hyp  DETAILS0.txt | tee RESULTS0.txt
 
-# subword-token NIER
+# subword-level TER & mTER & NIER
 ./error_rate  -m TER NID  -t $tokenizer  --lm $lm  --ref $ref  --hyp $hyp  DETAILS1.txt | tee RESULTS1.txt
 
 ## run examples in paper
