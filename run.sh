cb702b07b2cd3deda15ae71beed61c45413bdcbb
dophist <jerry.jiayu.du@gmail.com>
2022-11-15 00:54:44 +0800

    add Huffman Codec

--- run.sh
+++ run.sh
@@ -2,19 +2,25 @@
 
 if [[ ! -f lm/tokenizer.model ]]; then
     echo "Downloading language model for ASR evaluation ..."
+    # sentencepiece tokenizer & lm
     wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/tokenizer256/tokenizer.model -O lm/tokenizer256.model
     wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/lm256/4gram.trie -O lm/256_4gram.trie
 
     cd lm
     ln -s tokenizer256.model tokenizer.model
     cd -
+
+    # Huffman Codec word frequency
+    wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/word_count.tsv -O codec/word_count.tsv
 fi
 
 lm=lm/256_4gram.trie
 tokenizer=lm/tokenizer.model
 
-ref=ref.txt
-hyp=hyp.txt
+#ref=ref.txt
+#hyp=hyp.txt
+ref=data/gigaspeech/ref.txt
+hyp=data/gigaspeech/hyp.txt
 
 
 # word-level TER & mTER
@@ -22,6 +28,9 @@ hyp=hyp.txt
 
 # subword-level TER & mTER & NIER
 ./error_rate  -m TER NID  -t $tokenizer  --lm $lm  --ref $ref  --hyp $hyp  DETAILS1.txt | tee RESULTS1.txt
+
+# subword-level TER & mTER & NIER
+./error_rate  -m TER NID  -t word  --codec codec/word_count.tsv  --ref $ref  --hyp $hyp  DETAILS2.txt | tee RESULTS2.txt
 
 ## run examples in paper
 #./error_rate  -m TER  --ref data/paper/ref.txt  --hyp data/paper/hyp.txt  tmp.txt
