1fadf0a352d6b02a0fec637ea4a9a2c6a3113db7
dophist <jerry.jiayu.du@gmail.com>
2022-11-22 00:06:02 +0800

    simplify, remove codec

--- run.sh
+++ run.sh
@@ -2,20 +2,17 @@
 
 tokenizer=tokenizer_10k.model
 if [[ ! -f assets/$tokenizer ]]; then
-    echo "Downloading assets for ASR evaluation ..."
 	mkdir -p assets
     wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/$tokenizer  -O assets/$tokenizer
 fi
 
 ref=ref.txt
 hyp=hyp.txt
+#ref=data/gigaspeech/ref.txt
+#hyp=data/gigaspeech/hyp.txt
 
-# word-level TER/mTER
-./error_rate  -m TER  -t word  --ref $ref  --hyp $hyp  DETAILS0.txt | tee RESULTS0.txt
-
-# subword-level TER/mTER & NID
-./error_rate  -m TER NID  -t assets/$tokenizer  --codec spm:assets/$tokenizer  --ref $ref  --hyp $hyp  DETAILS1.txt | tee RESULTS1.txt
+# TER/mTER & NID
+./error_rate  -m TER NID  -t word  --ref $ref  --hyp $hyp  DETAILS.txt | tee RESULTS.txt
 
 ## run examples in paper
 #./error_rate  -m TER  --ref data/paper/ref.txt  --hyp data/paper/hyp.txt  tmp.txt
-
