dbc38bb8cefaea5771fb3c5fbd6ece3f4110dfd2
dophist <jerry.jiayu.du@gmail.com>
2022-11-22 17:49:43 +0800

    add wordcount codec

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 tokenizer=tokenizer_10k.model
+wordcount=wordcount.tsv
 if [[ ! -f assets/$tokenizer ]]; then
 	mkdir -p assets
     wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/$tokenizer  -O assets/$tokenizer
+    wget  https://swaphub.oss-cn-hangzhou.aliyuncs.com/asr_metric_assets/$wordcount  -O assets/$wordcount
 fi
 
 ref=ref.txt
@@ -12,7 +14,8 @@ hyp=hyp.txt
 #hyp=data/gigaspeech/hyp.txt
 
 # TER/mTER & NID
-./error_rate  -m TER NID  -t word  --ref $ref  --hyp $hyp  DETAILS.txt | tee RESULTS.txt
+./error_rate  -m TER NID  -t word  --ref $ref  --hyp $hyp  DETAILS0.txt | tee RESULTS0.txt
+./error_rate  -m TER NID  -t word  --codec count:assets/$wordcount  --ref $ref  --hyp $hyp  DETAILS1.txt | tee RESULTS1.txt
 
 ## run examples in paper
 #./error_rate  -m TER  --ref data/paper/ref.txt  --hyp data/paper/hyp.txt  tmp.txt
