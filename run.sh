e668c2bfbdb57c537ce88fe8ace6ed41e17836f1
dophist <jerry.jiayu.du@gmail.com>
2022-12-08 22:17:43 +0800

    add NID

--- run.sh
+++ run.sh
@@ -12,9 +12,9 @@ ref=data/gigaspeech/ref.txt
 hyp=data/gigaspeech/hyp.txt
 
 # TER/mTER & NIER
-#./error_rate  -m TER NIER -t word  --codec count:assets/$wordcount  --ref ref.txt  --hyp hyp.txt  DETAILS0.txt | tee RESULTS0.txt
-./error_rate  -m TER NIER -t word                                   --ref $ref  --hyp $hyp  DETAILS1.txt | tee RESULTS1.txt
-./error_rate  -m TER NIER -t word  --codec count:assets/$wordcount  --ref $ref  --hyp $hyp  DETAILS2.txt | tee RESULTS2.txt
+#./error_rate  -m TER NID NIER -t word  --codec count:assets/$wordcount  --ref ref.txt  --hyp hyp.txt  DETAILS0.txt | tee RESULTS0.txt
+./error_rate  -m TER NID NIER -t word                                   --ref $ref  --hyp $hyp  DETAILS1.txt | tee RESULTS1.txt
+./error_rate  -m TER NID NIER -t word  --codec count:assets/$wordcount  --ref $ref  --hyp $hyp  DETAILS2.txt | tee RESULTS2.txt
 
 ## run examples in paper
 #./error_rate  -m TER  -t word --ref data/paper/ref.txt  --hyp data/paper/hyp.txt  tmp.txt
